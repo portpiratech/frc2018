70f30a26563aaf79a658cf20d98d0d72506c981c
PortPiratech2 <portpiratech@gmail.com>
2018-02-12 18:57:20 -0600

    Trying to fix merge conflicts.

--- AdamSandler/src/org/usfirst/frc/team4804/robot/RobotMap.java
+++ AdamSandler/src/org/usfirst/frc/team4804/robot/RobotMap.java
@@ -15,6 +15,7 @@ public class RobotMap {
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
+	//Trying to fix merge conflicts
 	// public static int rangefinderModule = 1;
 	
 	//XBox controller IDs
